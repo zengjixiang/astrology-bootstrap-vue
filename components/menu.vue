cc23fc071e551bda7587fbdb94e4dfd8544ff980
zengjixiang <zengjixiang0401@gmail.com>
2021-09-12 23:30:37 +0800

    add menu、Tailwind CSS

---
+++ components/menu.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+
+    <div>
+      <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+
+      <NuxtLink to="/" class="justify">Home Page | </NuxtLink>
+
+      <NuxtLink to="/login" class="justify">Login Page | </NuxtLink>
+
+      <NuxtLink to="/info"  class="justify">Info Page | </NuxtLink>
+
+      <NuxtLink to="/about" class="justify">About Page | </NuxtLink>
+
+    </div>
+
+  </main>
+</template>
+
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,13 @@
 <template>
-  <Tutorial/>
+    <main>
+
+      <div class="flex">
+        <Menu/>
+          <h3 class="flex-center justify text-center">Index Page</h3>
+      </div>
+
+    </main>
+
 </template>
+
+
