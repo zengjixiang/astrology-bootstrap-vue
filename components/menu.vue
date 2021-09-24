8dcc0874f3c1162f825da3038c5acf9a6986eca6
zengjixiang <zengjixiang0401@gmail.com>
2021-09-24 22:25:37 +0800

    修改 bootstrap 版本和 menu，感覺兼容性沒有很好，之後會調整。

--- components/menu.vue
+++ components/menu.vue
@@ -1,7 +1,7 @@
 <template>
   <main>
-
     <div>
+
       <!-- tailwind.min.css -->
       <!-- <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet"> -->
 
@@ -10,24 +10,42 @@
 
       <!-- JavaScript Bundle with Popper -->
 
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous">
+      <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ"
+        crossorigin="anonymous"
+      />
 
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
+      <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+      />
 
       <NuxtLink to="/" class="justify">系統首頁 | </NuxtLink>
 
       <NuxtLink to="/login" class="justify">系統登入 | </NuxtLink>
 
-      <NuxtLink to="/info"  class="justify">占星資訊 | </NuxtLink>
+      <NuxtLink to="/planet" class="justify">行星資訊 | </NuxtLink>
+
+      <NuxtLink to="/asteroid" class="justify">小行星資訊 | </NuxtLink>
 
       <NuxtLink to="/about" class="justify">關於作者 | </NuxtLink>
-
     </div>
-
   </main>
 </template>
 
 <script>
+import Vue from "vue";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import "bootstrap/dist/js/bootstrap.bundle.js"
+Vue.use(BootstrapVue);
+
+
 </script>
 
+<script setup>
 
+
+</script>
---
+++ pages/asteroid.vue
@@ -0,0 +1,10 @@
+<template>
+  <main>
+
+    <div>
+      <Menu/>
+      <h3 class="flex-center justify text-center">小行星資訊</h3>
+    </div>
+
+  </main>
+</template>
